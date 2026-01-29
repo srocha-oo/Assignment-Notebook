e2ec165c7877192f7362d8b38316d62aa47c0069
Stephanie Rocha M <srochamarquez@bsd220.org>
2026-01-29 14:12:19 -0600

    Added assignment items

--- Assignment Notebook/ContentView.swift
+++ Assignment Notebook/ContentView.swift
@@ -8,17 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    struct AssignmentItem: Identifiable {
+           let id = UUID()
+           var course: String
+           var description: String
+           var dueDate: Date
+       }
+    @State private var AssignmentItems = [
+            AssignmentItem(course: "Algebra", description: "Linear Equations", dueDate: Date()),
+            AssignmentItem(course: "Science", description: "Atomic Bomb Lab", dueDate: Date()),
+            AssignmentItem(course: "History", description: "Civil War Essay", dueDate: Date()),
+            AssignmentItem(course: "English", description: "AP Central Assignment", dueDate: Date())
+        ]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                ForEach(AssignmentItems) { thing in Text(thing.description) }
+                .onMove(perform: { indices, newOffset in
+                    AssignmentItems.move(fromOffsets: indices, toOffset: newOffset)
+                })
+                .onDelete(perform: { indexSets in
+                    AssignmentItems.remove(atOffsets: indexSets)
+                })
+                .navigationBarTitle("Things", displayMode: .inline)
+                .navigationBarItems(leading: EditButton())
+            }
         }
-        .padding()
     }
 }
-
 #Preview {
     ContentView()
 }
