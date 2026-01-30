0591903d8bc7427990191cfc1d2b037be2044c4b
Stephanie Rocha M <srochamarquez@bsd220.org>
2026-01-30 09:58:42 -0600

    Step 6

--- Assignment Notebook/ContentView.swift
+++ Assignment Notebook/ContentView.swift
@@ -9,32 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     struct AssignmentItem: Identifiable {
-           let id = UUID()
-           var course: String
-           var description: String
-           var dueDate: Date
-       }
-    @State private var AssignmentItems = [
-            AssignmentItem(course: "Algebra", description: "Linear Equations", dueDate: Date()),
-            AssignmentItem(course: "Science", description: "Atomic Bomb Lab", dueDate: Date()),
-            AssignmentItem(course: "History", description: "Civil War Essay", dueDate: Date()),
-            AssignmentItem(course: "English", description: "AP Central Assignment", dueDate: Date())
-        ]
+        let id = UUID()
+        var course: String
+        var description: String
+        var dueDate: Date
+    }
+    @State private var assignmentItems = [
+        AssignmentItem(course: "Algebra", description: "Linear Equations", dueDate: Date()),
+        AssignmentItem(course: "Science", description: "Atomic Bomb Lab", dueDate: Date()),
+        AssignmentItem(course: "History", description: "Civil War Essay", dueDate: Date()),
+        AssignmentItem(course: "English", description: "AP Central Assignment", dueDate: Date())
+    ]
     var body: some View {
         NavigationView {
             List {
-                ForEach(AssignmentItems) { thing in Text(thing.description) }
+                ForEach(assignmentItems) { item in
+                    VStack(alignment: .leading) {
+                        Text(item.course).font(.headline)
+                        Text(item.description)
+                    }
+                }
                 .onMove(perform: { indices, newOffset in
-                    AssignmentItems.move(fromOffsets: indices, toOffset: newOffset)
+                    assignmentItems.move(fromOffsets: indices, toOffset: newOffset)
                 })
                 .onDelete(perform: { indexSets in
-                    AssignmentItems.remove(atOffsets: indexSets)
+                    assignmentItems.remove(atOffsets: indexSets)
                 })
-                .navigationBarTitle("Things", displayMode: .inline)
-                .navigationBarItems(leading: EditButton())
+                
             }
+            .navigationBarTitle("Assignment Noteboook", displayMode: .inline)
+            .navigationBarItems(leading: EditButton())
         }
-        
     }
 }
 #Preview {
